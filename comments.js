97bc152c15218806d8fa410050103dc4ef607c08
Holden Riefenstahl <hrief@protonmail.com>
2023-11-01 00:52:30 -0400

    modified copilot third commit

--- comments.js
+++ comments.js
@@ -1,101 +1,109 @@
 // create web server
-var express = require('express');
-var app = express();
-// import body-parser module
-var bodyParser = require('body-parser');
-// import mysql module
-var mysql = require('mysql');
-// import express-session module
-var session = require('express-session');
-// import multer module
-var multer = require('multer');
-// import fs module
-var fs = require('fs');
-// import path module
-var path = require('path');
-// import crypto module
-var crypto = require('crypto');
-// import passport module
-var passport = require('passport');
-// import passport-local module
-var LocalStrategy = require('passport-local').Strategy;
-// import passport-facebook module
-var FacebookStrategy = require('passport-facebook').Strategy;
-// import passport-twitter module
-var TwitterStrategy = require('passport-twitter').Strategy;
-// import passport-google module
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+// use express
+// use body-parser
+// use express-session
+// use cookie-parser
+// use mongoose
+// use connect-flash
+// use express-handlebars
+// use express-validator
+// use passport
+// use passport-local
+// use passport-local-mongoose
+// use connect-mongo
+// use multer
+// use morgan
+// use method-override
+// use moment
 
-// create mysql connection
-var conn = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '1234',
-	database: 'o2'
-});
+// Path: app.js
+// require comments.js
+// require users.js
+// require index.js
 
-// connect to mysql server
-conn.connect();
+// Path: comments.js
+// require express
+// require express-session
+// require body-parser
+// require cookie-parser
+// require mongoose
+// require multer
+// require morgan
+// require method-override
+// require moment
+// require connect-mongo
+// require express-handlebars
+// require express-validator
+// require passport
+// require passport-local
+// require passport-local-mongoose
+// require connect-flash
 
-// use body-parser module
-app.use(bodyParser.urlencoded({extended:false}));
+// Path: users.js
+// require express
+// require express-session
+// require body-parser
+// require cookie-parser
+// require mongoose
+// require multer
+// require morgan
+// require method-override
+// require moment
+// require connect-mongo
+// require express-handlebars
+// require express-validator
+// require passport
+// require passport-local
+// require passport-local-mongoose
+// require connect-flash
 
-// use express-session module
-app.use(session({
-	secret: 'keyboard cat',
-	resave: false,
-	saveUninitialized: true
-}));
+// Path: index.js
+// require express
+// require express-session
+// require body-parser
+// require cookie-parser
+// require mongoose
+// require multer
+// require morgan
+// require method-override
+// require moment
+// require connect-mongo
+// require express-handlebars
+// require express-validator
+// require passport
+// require passport-local
+// require passport-local-mongoose
+// require connect-flash
 
-// use passport module
-app.use(passport.initialize());
-// use passport session module
-app.use(passport.session());
+// Path: public
+// create folder
+// create folder images
+// create folder css
+// create folder js
+// create folder fonts
+// create folder libs
 
-// use multer module
-app.use('/user', express.static('uploads'));
+// Path: public/images
+// create folder posts
+// create folder users
 
-// set storage
-var storage = multer.diskStorage({
-	// destination for files
-	destination: function(req, file, callback) {
-		callback(null, './uploads');
-	},
-	// add back the extension
-	filename: function(req, file, callback) {
-		callback(null, Date.now() + path.extname(file.originalname));
-	}
-});
+// Path: public/css
+// create folder libs
+// create folder posts
+// create folder users
+// create folder partials
 
-// upload parameters for multer
-var upload = multer({
-	storage: storage
-});
+// Path: public/js
+// create folder libs
+// create folder posts
+// create folder users
+// create folder partials
 
-// passport serialize
-passport.serializeUser(function(user, done) {
-	console.log('passport session save : ', user);
-	done(null, user);
-});
-// passport deserialize
-passport.deserializeUser(function(user, done) {
-	console.log('passport session get id : ', user);
-	done(null, user);
-});
+// Path: public/fonts
+// create folder libs
+// create folder posts
+// create folder users
+// create folder partials
 
-// create local strategy
-passport.use('local-login', new LocalStrategy({
-		usernameField: 'email',
-		passwordField: 'password',
-		passReqToCallback : true
-	}, function(req, email) {
-        var query = conn.query('select * from user where email=?', [email], function(err, rows) {
-            if(err) return done(err);
-
-            if(rows.length) {
-                return done(null, {'email': email, 'id': rows[0].uid});
-            } else {
-                return done(null, false, {'message': 'your login info is not found'});
-            }
-        });
-    }));
+// Path: public/libs
+// create folder
